a7d9508c32dbbb2e73f5aaafe9a51f854bb1d2c3
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2021-04-12 17:52:40 +0100

    Reduce code-reuse

--- register/registers.py
+++ register/registers.py
@@ -11,26 +11,18 @@ class Register(collections.OrderedDict):
             def wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
 
-            self[wrapper] = (args, kwargs)
+            self[wrapper] = self._parse(*args, **kwargs)
 
             return wrapper
 
         return decorator
+
+    def _parse(self, *args, **kwargs):
+        return (args, kwargs)
 
 @attr.s
 class ObjectRegister(Register):
     type: type = attr.ib()
 
-    def __call__(self, *args, **kwargs):
-        obj = self.type(*args, **kwargs)
-
-        def decorator(func):
-            @functools.wraps(func)
-            def wrapper(*args, **kwargs):
-                return func(*args, **kwargs)
-
-            self[wrapper] = obj
-
-            return wrapper
-
-        return decorator
+    def _parse(self, *args, **kwargs):
+        return self.type(*args, **kwargs)
